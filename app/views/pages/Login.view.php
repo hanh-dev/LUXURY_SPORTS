2755e83a0611ee74684e67a9c38cb06f3449caad
hoviettien <tien.ho26@student.passerellesnumeriques.org>
2024-12-11 15:50:49 +0700

    complete login/html

---
+++ app/views/pages/Login.view.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container-login">
+        <div>
+            <form action="">
+                <div>
+                    <span>LOGIN</span>
+                    <i class="fa-solid fa-xmark"></i>
+                </div>
+                <div>
+                    <input type="text" placeholder="Username">
+                </div>
+                <div>
+                    <input type="text" placeholder="Password">
+                </div>
+                <div>
+                    <input type="checkbox">
+                    <p>Remember me</p>
+                </div>
+                <div>
+                    <button>Log In</button>
+                </div>
+                <a href="">Lost your password?</a>
+                <div>
+                    <p>Not a memeber?</p>
+                    <a href="">Register</a>
+                </div>
+            </form>
+        </div>
+    </div>
+</body>
+</html>
