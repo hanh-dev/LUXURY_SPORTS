d5a70fe179926195e507622830c21b7dd2af426f
Hanh_Dev Clean <hanh.ho26@student.passerellesnumeriques.org>
2024-12-11 10:40:56 +0700

    feat(header): setup header layout, pending style updates

---
+++ app/views/components/Footer.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <base href="/LUXURY_SPORTS/app/">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Footer</title>
+</head>
+<body>
+    <h1>Footer</h1>
+</body>
+</html>
