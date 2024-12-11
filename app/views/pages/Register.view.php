161891223c72875efa0a9da6296016750e202f11
hoviettien <tien.ho26@student.passerellesnumeriques.org>
2024-12-11 15:06:59 +0700

    complete register/html

---
+++ app/views/pages/Register.view.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container-register">
+        <div>
+            <form action="">
+                <div>
+                    <span>REGISTER</span>
+                    <i class="fa-solid fa-xmark"></i>
+                </div>
+                <div>
+                    <input type="text" placeholder="Email address">
+                </div>
+                <div>
+                    <input type="text" placeholder="Username">
+                </div>
+                <div>
+                    <input type="text" placeholder="Password">
+                </div>
+                <div>
+                    <input type="text" placeholder="Confirm password">
+                </div>
+                <h4>Please ensure your information!</h4>
+                <div>
+                    <button>Register</button>
+                </div>
+                <div>
+                    <p>Aready a member?</p>
+                    <a href="">Login</a>
+                </div>
+            </form> 
+        </div>
+    </div>
+</body>
+</html>
