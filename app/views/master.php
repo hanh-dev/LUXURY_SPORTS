35e6d60ce7c789413e68b4071d8a4c87b7a4bd74
ChăngSeven2k5 <changcoor7@gmail.com>
2024-12-13 07:48:42 +0700

    Fix CSS link path

--- app/views/master.php
+++ app/views/master.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <base href="/MVC_PROJECT/LUXURY_SPORTS/">
+    <base href="/LUXURY_SPORTS/">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
