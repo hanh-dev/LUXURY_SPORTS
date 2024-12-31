82a079bf531868438acc143556788a98886dd060
hoviettien <tien.ho26@student.passerellesnumeriques.org>
2024-12-31 15:21:12 +0700

    complete AboutUs page / css

--- app/views/pages/AboutUs.view.php
+++ app/views/pages/AboutUs.view.php
@@ -3,29 +3,30 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="public/css/AboutUs.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.0/css/all.min.css" integrity="sha512-9xKTRVabjVeZmc+GUW8GgSmcREDunMM+Dt/GrzchfN8tkwHizc5RP4Ok/MXFFy5rIjJjzhndFScTceq5e6GvVQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Document</title>
 </head>
 <body>
     <div class="container">
         <div class="box-img">
-            <img src="https://cdn.benhvienthucuc.vn/wp-content/uploads/2024/10/tac-dung-cua-chay-bo-doi-voi-than.jpg" alt="">
+            <img src="https://cdn.tgdd.vn/Files/2020/10/22/1301202/xx-tac-dung-tuyet-voi-cua-chay-bo-doi-voi-suc-khoe.png" alt="">
         </div>
         <div class="text">
             <div class="text1"><b>About Luxury Sports</b></div>
             <div class="text2">
-                <div>Luxury Sports Roles</div>
-                <div>Career FAQ's</div>
-                <div>Affilate Program</div>
+                <div><b>Luxury Sports Roles</b></div>
+                <div><b>Career FAQ's</b></div>
+                <div><b>Affilate Program</b></div>
             </div>
         </div>
         <div class="card">
             <div class="card-img">
-                <img src="https://84race.com/public/media//file-20210127-17-if809z.jpg" alt="">
+                <img src="https://thanhnien.mediacdn.vn/Uploaded/camlt/2022_03_30/chay-bo-kyfk-3330.jpg" alt="">
             </div>
             <div class="box-content">
                 <div class="title">Lorem ipsum dolor, sit amet consectetur adipisicing.</div>
-                <div class="content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut molestias impedit repellendus laudantium corrupti voluptates architecto. Quia sint est quaerat laborum autem at nostrum, expedita quo. Eius non aperiam id deleniti corporis nihil reprehenderit eum voluptatibus, praesentium, enim temporibus quas?</div>
+                <div class="content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut molestias corrupti voluptates architecto. Quia sint est quaerat laborum autem at nostrum, expedita quo. Eius non aperiam id deleniti corporis nihil reprehenderit eum voluptatibus, praesentium, enim temporibus quas?</div>
                 <div class="content-line">
                     <div class="line">
                         <div class="icon"><i class="fa-solid fa-check"></i></div>
@@ -45,8 +46,8 @@
         <div class="team"><b>Meet the our team</b></div>
         <div class="box-team">
             <div class="card-team">
-                <div class="box-img">
-                    <img src="" alt="">
+                <div class="box-img-team">
+                    <img src="public/images/cv.jpg" alt="">
                 </div>
                 <div class="box-name">
                     <p class="name"><b>Ho Van Hanh</b></p>
@@ -54,7 +55,7 @@
                 </div>
             </div>
             <div class="card-team">
-                <div class="box-img">
+                <div class="box-img-team">
                     <img src="" alt="">
                 </div>
                 <div class="box-name">
@@ -63,8 +64,8 @@
                 </div>
             </div>
             <div class="card-team">
-                <div class="box-img">
-                    <img src="" alt="">
+                <div class="box-img-team">
+                    <img src="public/images/anh_cv.jpg" alt="">
                 </div>
                 <div class="box-name">
                     <p class="name"><b>Ho Viet Tien</b></p>
