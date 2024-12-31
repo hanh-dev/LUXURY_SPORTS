9ed729d6dbc03e87a527a562f98ddc079ad9b22b
hoviettien <tien.ho26@student.passerellesnumeriques.org>
2024-12-31 11:01:15 +0700

    complete AboutUs page / html

--- app/views/pages/AboutUs.view.php
+++ app/views/pages/AboutUs.view.php
@@ -1,5 +1,77 @@
-<h2>About Us Hello</h2>
-<h2>About Us Hello</h2>
-<h2>About Us Hello</h2>
-<h2>About Us Hello</h2>
-<h2>About Us Hello</h2>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.0/css/all.min.css" integrity="sha512-9xKTRVabjVeZmc+GUW8GgSmcREDunMM+Dt/GrzchfN8tkwHizc5RP4Ok/MXFFy5rIjJjzhndFScTceq5e6GvVQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div class="box-img">
+            <img src="https://cdn.benhvienthucuc.vn/wp-content/uploads/2024/10/tac-dung-cua-chay-bo-doi-voi-than.jpg" alt="">
+        </div>
+        <div class="text">
+            <div class="text1"><b>About Luxury Sports</b></div>
+            <div class="text2">
+                <div>Luxury Sports Roles</div>
+                <div>Career FAQ's</div>
+                <div>Affilate Program</div>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-img">
+                <img src="https://84race.com/public/media//file-20210127-17-if809z.jpg" alt="">
+            </div>
+            <div class="box-content">
+                <div class="title">Lorem ipsum dolor, sit amet consectetur adipisicing.</div>
+                <div class="content">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ut molestias impedit repellendus laudantium corrupti voluptates architecto. Quia sint est quaerat laborum autem at nostrum, expedita quo. Eius non aperiam id deleniti corporis nihil reprehenderit eum voluptatibus, praesentium, enim temporibus quas?</div>
+                <div class="content-line">
+                    <div class="line">
+                        <div class="icon"><i class="fa-solid fa-check"></i></div>
+                        <div class="text-line">Lorem ipsum dolor sit amet consectetur adipisicing.</div>
+                    </div>
+                    <div class="line">
+                        <div class="icon"><i class="fa-solid fa-check"></i></div>
+                        <div class="text-line">Lorem ipsum dolor sit amet. Minima, cumque.</div>
+                    </div>
+                    <div class="line">
+                        <div class="icon"><i class="fa-solid fa-check"></i></div>
+                        <div class="text-line">Lorem ipsum dolor sit amet.</div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="team"><b>Meet the our team</b></div>
+        <div class="box-team">
+            <div class="card-team">
+                <div class="box-img">
+                    <img src="" alt="">
+                </div>
+                <div class="box-name">
+                    <p class="name"><b>Ho Van Hanh</b></p>
+                    <p class="vitri">Leader</p>
+                </div>
+            </div>
+            <div class="card-team">
+                <div class="box-img">
+                    <img src="" alt="">
+                </div>
+                <div class="box-name">
+                    <p class="name"><b>Coor Chang</b></p>
+                    <p class="vitri">Member</p>
+                </div>
+            </div>
+            <div class="card-team">
+                <div class="box-img">
+                    <img src="" alt="">
+                </div>
+                <div class="box-name">
+                    <p class="name"><b>Ho Viet Tien</b></p>
+                    <p class="vitri">Member</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
