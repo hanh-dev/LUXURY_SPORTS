1482cbc349654e6a9f2e0e994416ae84c6b6f60a
hoviettien <tien.ho26@student.passerellesnumeriques.org>
2025-01-01 21:11:58 +0700

    LS-97 complete Contact page / html

--- app/views/pages/ContactUs.view.php
+++ app/views/pages/ContactUs.view.php
@@ -3,13 +3,54 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.0/css/all.min.css" integrity="sha512-9xKTRVabjVeZmc+GUW8GgSmcREDunMM+Dt/GrzchfN8tkwHizc5RP4Ok/MXFFy5rIjJjzhndFScTceq5e6GvVQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Document</title>
 </head>
 <body>
-    <h1>Hello Contact</h1>
-    <h1>Hello Contact</h1>
-    <h1>Hello Contact</h1>
-    <h1>Hello Contact</h1>
-    <h1>Hello Contact</h1>
-</body>
+    <div class="container">
+        <div class="box-img">
+            <img src="https://www.hipotels.com/content/thumbs/640/content/imgsxml/panel_menufotos/bahiagrande-tenis220.jpg" alt="">
+        </div>
+        <div class="box-contact"> 
+            <div class="card">
+                <div class="icon"><i class="fa-solid fa-location-dot"></i></div>
+                <div class="content">
+                    <h2>Store Address</h2>
+                    <h3>LUXURY SPORTS 123 To Hien Thanh Street, Son Tra, Da Nang</h3>
+                </div>
+            </div>
+            <div class="card">
+                <div class="icon"><i class="fa-solid fa-phone-volume"></i></div>
+                <div class="content">
+                    <h2>Contact Info</h2>
+                    <h3>People please open a chat or give us call at (+84) 923864321</h3>
+                </div>
+            </div>
+            <div class="card">
+                <div class="icon"><i class="fa-solid fa-message"></i></div>
+                <div class="content">
+                    <h2>Live Support</h2>
+                    <h3>People can also contact the store live support http://www.livechat.com</h3>
+                </div>
+            </div>
+        </div>
+        <div class="box-input">
+            <div class="text">If you have any needs or questions, please contact us by entering the information below</div>
+            <div class="input">
+                <div class="input1">
+                    <input type="text" placeholder="Enter Your Name">
+                </div>
+                <div class="input2">
+                    <input type="text" placeholder="Enter Your Email">
+                </div>
+                <div class="input3">
+                    <input type="text" placeholder="Enter Your Address">
+                </div>
+                <div class="input4">
+                    <input type="text" placeholder="Enter Your Content">
+                </div>
+                <div class="send">Send Message</div>
+            </div>
+        </div>
+    </div>
 </html>
